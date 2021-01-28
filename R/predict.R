b14012b7586cdd38b2dc5d1a3630c722edb2e037
Alan Inglis <alan.n.inglis@gmail.com>
2021-01-28 13:56:54 +0000

    used styler

--- R/predict.R
+++ R/predict.R
@@ -1,15 +1,18 @@
-CVpredictfun <- function(classif=FALSE, class=1){
-  if (classif)
-    function(fit, data){
-      pred <- tryCatch(CVpredict(fit,data, ptype="probmatrix")[,class],
-                       error = function(e) NULL,warning = function(w) {})
-      if (is.null(pred)){
-        pred <- CVpredict(fit,data)
+#' @export
+CVpredictfun <- function(classif = FALSE, class = 1) {
+  if (classif) {
+    function(fit, data) {
+      pred <- tryCatch(CVpredict(fit, data, ptype = "probmatrix")[, class],
+        error = function(e) NULL, warning = function(w) {}
+      )
+      if (is.null(pred)) {
+        pred <- CVpredict(fit, data)
         if (is.numeric(class) & is.factor(pred)) class <- levels(pred)[class]
-        pred <- as.numeric(pred==class)
+        pred <- as.numeric(pred == class)
       }
       pred
     }
-  else CVpredict
+  } else {
+    CVpredict
+  }
 }
-
