86b2f959bb674d5a2c0f32cf35f6de2c7b90843a
Alan Inglis <alan.n.inglis@gmail.com>
2021-02-15 18:50:03 +0000

    matrix to dataframe function

--- R/matrix2df.R
+++ R/matrix2df.R
@@ -18,34 +18,33 @@
 #' myMat <- vivi(fit = rF, data = aq, response = "Ozone")
 #' myDf <- matrix2df(myMat)
 #' myDf
-#'
 #' @export
 
 
 # Turn matrix into df with index ------------------------------------------
 
-matrix2df <- function(matrix){
+matrix2df <- function(matrix) {
 
-# melt the matrix
-df <- melt(matrix)
-# get the row and colum index
-ind <- which(!is.na(matrix), arr.ind = TRUE)
-# get position of importance values
-impIndex = as.integer(df$X1 == df$X2)
-# get position of interaction values
-intIndex = 1 - impIndex
-# set Vimp values
-Vimp = impIndex * df$value
-# set Vint values
-Vint = intIndex * df$value
-# cbind them together
-viviDataFrame <- cbind(df[-(2:3)], Vimp, Vint, ind)
-# convert to dataframe
-viviDataFrame <- as.data.frame(viviDataFrame)
-# set names
-rownames(viviDataFrame) <- NULL
-colnames(viviDataFrame)[1] <- "Variable"
+  # melt the matrix
+  df <- melt(matrix)
+  # get the row and colum index
+  ind <- which(!is.na(matrix), arr.ind = TRUE)
+  # get position of importance values
+  impIndex <- as.integer(df$X1 == df$X2)
+  # get position of interaction values
+  intIndex <- 1 - impIndex
+  # set Vimp values
+  Vimp <- impIndex * df$value
+  # set Vint values
+  Vint <- intIndex * df$value
+  # cbind them together
+  viviDataFrame <- cbind(df[-(3)], Vimp, Vint, ind)
+  # convert to dataframe
+  viviDataFrame <- as.data.frame(viviDataFrame)
+  # set names
+  rownames(viviDataFrame) <- NULL
+  colnames(viviDataFrame)[1] <- "Variable_1"
+  colnames(viviDataFrame)[2] <- "Variable_2"
 
-return(viviDataFrame)
-
+  return(viviDataFrame)
 }
