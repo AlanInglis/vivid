5843016970e64171c47c083caab183cf0bfd436a
Alan Inglis <alan.n.inglis@gmail.com>
2021-03-19 17:57:10 +0000

    package updates

---
+++ R/zzz.R
@@ -0,0 +1,6 @@
+utils::globalVariables(c("r1",
+                         ".id",
+                         "pred",
+                         "Variable_1",
+                         "Variable_2",
+                         "Value"))
