464734d910b80c7cfdb78f1de119ba15486c3361
Alan Inglis <alan.n.inglis@gmail.com>
2021-05-13 17:52:50 +0100

    added h2o pdpPairs test scripts

---
+++ paperCodeData/pdpPairsTest.R
@@ -0,0 +1,69 @@
+library(ggplot2)
+library(condvis2)
+library(vivid)
+library(GGally)
+library(dplyr)
+library(h2o)
+
+
+h2o.init()
+h2o.no_progress()
+
+
+# predict function --------------------------------------------------------
+
+CVpredictfun <- function(classif = FALSE, class = 1) {
+  if (classif) {
+    function(fit, data) {
+      pred <- tryCatch(CVpredict(fit, data, ptype = "probmatrix")[, class],
+                       error = function(e) NULL, warning = function(w) {}
+      )
+      if (is.null(pred)) {
+        pred <- CVpredict(fit, data)
+        if (is.numeric(class) & is.factor(pred)) class <- levels(pred)[class]
+        pred <- as.numeric(pred == class)
+      }
+      pred
+    }
+  } else {
+    CVpredict
+  }
+}
+
+
+
+# air quality -------------------------------------------------------------
+
+
+aq <- na.omit(airquality)
+x <- names(aq[,-1])
+y <- "Ozone"
+
+
+
+set.seed(1234)
+aqDL <- h2o.deeplearning(x,
+                           y,
+                           as.h2o(aq)
+)
+
+
+pdpPairsH2O(aq, aqDL, "Ozone")
+
+# iris --------------------------------------------------------------------
+# data
+y <- "Species"
+x <- names(iris[,-5])
+
+set.seed(1234)
+irisDL <- h2o.deeplearning(x,
+                           y,
+                           as.h2o(iris)
+)
+
+
+pdpPairsH2O(iris, irisDL, "Species")
+
+
+
+
