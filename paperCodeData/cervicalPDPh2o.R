16fb49842d058e8224eed864d38ea55a0bd1255e
Alan Inglis <alan.n.inglis@gmail.com>
2021-05-14 12:40:35 +0100

    added h2o pdp scripts

--- paperCodeData/cervicalPDPh2o.R
+++ paperCodeData/cervicalPDPh2o.R
@@ -6,6 +6,34 @@ library(h2o)
 library(vivid)
 library(imbalance)
 
+
+
+# predict function --------------------------------------------------------
+
+CVpredictfun <- function(classif = FALSE, class = 1) {
+  if (classif) {
+    function(fit, data) {
+      pred <- tryCatch(CVpredict(fit, data, ptype = "probmatrix")[, class],
+                       error = function(e) NULL, warning = function(w) {}
+      )
+      if (is.null(pred)) {
+        pred <- CVpredict(fit, data)
+        if (is.numeric(class) & is.factor(pred)) class <- levels(pred)[class]
+        pred <- as.numeric(pred == class)
+      }
+      pred
+    }
+  } else {
+    CVpredict
+  }
+}
+
+
+
+
+# -------------------------------------------------------------------------
+
+
 # get data
 cervicalReal <- read.csv("/Users/alaninglis/Desktop/cervicalCancerCopy.csv", sep=",",  na.strings = c('?'), stringsAsFactors = FALSE)
 
