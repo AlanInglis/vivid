241038023511a0c07894ee966c8449542037f3c2
Alan Inglis <alan.n.inglis@gmail.com>
2021-06-20 00:45:30 +0100

    updated paper code

--- paperCodeData/correlatedVariablesTests.R
+++ paperCodeData/correlatedVariablesTests.R
@@ -1,13 +1,14 @@
 library(ranger)
 library(vivid)
 library(MASS)
+library(ggplot2)
 
 set.seed(1701)
 
 # Normalised Vs Un-Normalised ----------------------------------------------------------
 
 # Data:
-fData <- genFriedman(noFeatures = 10, noSamples = 1000)
+fData <- genFriedman(noFeatures = 10, noSamples = 1000, seed = 1701)
 
 # Fit:
 fFit <- ranger(y ~ ., data = fData, importance = "permutation")
@@ -36,36 +37,61 @@ mvnData <- data.frame(
   X2 = runif(samples, 0 ,1),
   X3 = runif(samples, 0, 1),
   X4 = corVals[, 1],
-  X5 = corVals[, 2]
+  X5 = corVals[, 2],
+  X6 = runif(samples, 0 ,1),
+  X7 = runif(samples, 0 ,1),
+  X8 = runif(samples, 0, 1),
+  X9 = runif(samples, 0 ,1),
+  X10 = runif(samples, 0 ,1)
 )
 
 # Check correlations
-cor(mvnData)
+corrplot::corrplot(cor(mvnData), type = "lower")
 
 # Equation:
 y = (10*sin(pi*mvnData$X1*mvnData$X2) + 20 * (mvnData$X3-0.5)^2 + 10 * mvnData$X4 + 5 * mvnData$X5 + e)
 mvnData$y <- y
 
-# Create random forest model:
-mvnFit <- ranger(y~., data = mvnData, importance = "permutation")
+
+# Visualisations ----------------------------------------------------------
+# Fit:
+fFitCorr <- ranger(y ~ ., data = mvnData, importance = "permutation")
 
 # vivi Normalized & Unnormalized:
-mvnNormF <- vivi(fit = mvnFit, data = mvnData, response = "y", normalized = FALSE)
-mvnNormT <- vivi(fit = mvnFit, data = mvnData, response = "y", normalized = TRUE)
+fNormalTCorr <- vivi(fit = fFitCorr, data = mvnData, response = "y", normalized = TRUE)
+fNormalFCorr <- vivi(fit = fFitCorr, data = mvnData, response = "y", normalized = FALSE)
 
-# Visualisations:
-viviHeatmap(mvnNormF)
-viviHeatmap(mvnNormT)
+# Heatmap:
+viviHeatmap(fNormalTCorr)
+viviHeatmap(fNormalFCorr)
+
+# Look at individual PDPs -------------------------------------------------
 
 # Create models with & without influence of correlated variables:
-mvnFit_1 <- ranger(y~ X1 + X2 + X3 + X4, data = mvnData, importance = "permutation")
-mvnFit_2 <- ranger(y~ X1 + X2 + X3 + X5, data = mvnData, importance = "permutation")
+mvnFit_both <- ranger(y~ X3 + X4 + X5 + X6 + X7 + X8 + X9 + X10, data = mvnData, importance = "permutation")
+mvnFit_X4 <- ranger(y~ X3 + X4 + X6 + X7 + X8 + X9 + X10, data = mvnData, importance = "permutation")
+mvnFit_X5 <- ranger(y~ X3 + X5 + X6 + X7 + X8 + X9 + X10, data = mvnData, importance = "permutation")
 
 # Visualise PDPs for each scenario:
-pdpVars(mvnData, mvnFit,   "y", nIce = 0, vars = c("X4"), limits = c(-30, 45))
-pdpVars(mvnData, mvnFit,   "y", nIce = 0, vars = c("X5"), limits = c(-30, 45))
-pdpVars(mvnData, mvnFit_1, "y", nIce = 0, vars = c("X4"), limits = c(-30, 45))
-pdpVars(mvnData, mvnFit_2, "y", nIce = 0, vars = c("X5"), limits = c(-30, 45))
+bothX4 <- pdpVars(mvnData, mvnFit_both, "y", nIce = 0, vars = c("X4"), limits = c(-20, 40))
+bothX5 <- pdpVars(mvnData, mvnFit_both, "y", nIce = 0, vars = c("X5"), limits = c(-20, 40))
+justX4 <- pdpVars(mvnData, mvnFit_X4, "y", nIce = 0,   vars = c("X4"), limits = c(-20, 40))
+justX5 <- pdpVars(mvnData, mvnFit_X5, "y", nIce = 0,   vars = c("X5"), limits = c(-20, 40))
 
+# create overlaid plot for X4:
+bothX4[[1]]$layers[[2]]$aes_params$colour <- "red"
+pX4 <- bothX4[[1]] + justX4[[1]]$layers
+aX4 <- annotate("text", x = 2.5, y = 18, label = "Model 3", vjust = -2, hjust = 0.2, colour = "red")
+aX4_1 <- annotate("text", x = 2.5, y = 33, label = "Model 1", vjust = -2, hjust = 0.2, colour = "black")
+pX4 + aX4 + aX4_1
+
+# create overlaid plot for X5:
+bothX5[[1]]$layers[[2]]$aes_params$colour <- "red"
+pX5 <- bothX5[[1]] + justX5[[1]]$layers
+aX5 <-  annotate("text", x = 2, y = 12, label = "Model 3", vjust = -2, hjust = 0.2, colour = "red")
+aX5_1 <- annotate("text", x = 2, y = 31, label = "Model 2", vjust = -2, hjust = 0.2, colour = "black")
+pX5 + aX5 + aX5_1
+
+# -------------------------------------------------------------------------
 
 
