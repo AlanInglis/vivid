4ac4eca7da81d2f7e51667b06600e78075ffdbe5
Alan Inglis <alaninglis@MAC-9DD782.station>
2025-08-22 17:58:06 +0100

    added geom_encircle function

--- R/geom_encircle.R
+++ R/geom_encircle.R
@@ -3,20 +3,23 @@
 # for the now defunct package ggalt. Thanks Ben!
 
 
-
+#' @importFrom grid grobTree gpar
 draw_key_hack <- function(data, params, size) {
   data$fill <- alpha(data$fill, data$alpha)
   data$alpha <- 1
 
-  grobTree(
+  grid::grobTree(
     if (!is.na(data$fill)) grid::rectGrob(gp = gpar(col = NA, fill = data$fill)),
     draw_key_path(data, params)
   )
 }
 
 
-#' @format NULL
-#' @usage NULL
+#' Geom for drawing encircling polygons around groups
+#'
+#' This geom is used internally by [geom_encircle_vivi()], but is exported
+#' so that users can add it directly if desired.
+#'
 #' @export
 GeomEncircle <- ggproto("GeomEncircle", Geom,
                         required_aes = c("x", "y"),
@@ -191,36 +194,17 @@ if (FALSE) {
 #' @return adds a circle around the specified points
 #' @export
 #' @examples
+#' library(ggplot2)
 #' d <- data.frame(x=c(1,1,2),y=c(1,2,2)*100)
 #'
-#' gg <- ggplot(d,aes(x,y))
+#' gg <- ggplot2::ggplot(d,aes(x,y))
 #' gg <- gg + scale_x_continuous(expand=c(0.5,1))
 #' gg <- gg + scale_y_continuous(expand=c(0.5,1))
 #'
-#' gg + geom_encircle(s_shape=1, expand=0) + geom_point()
+#' gg + geom_encircle_vivi(s_shape=1, expand=0) + geom_point()
 #'
-#' gg + geom_encircle(s_shape=1, expand=0.1, colour="red") + geom_point()
-#'
-#' gg + geom_encircle(s_shape=0.5, expand=0.1, colour="purple") + geom_point()
-#'
-#' gg + geom_encircle(data=subset(d, x==1), colour="blue", spread=0.02) +
-#'   geom_point()
-#'
-#' gg +geom_encircle(data=subset(d, x==2), colour="cyan", spread=0.04) +
-#'   geom_point()
-#'
-#' gg <- ggplot(mpg, aes(displ, hwy))
-#' gg + geom_encircle(data=subset(mpg, hwy>40)) + geom_point()
-#' gg + geom_encircle(aes(group=manufacturer)) + geom_point()
-#' gg + geom_encircle(aes(group=manufacturer,fill=manufacturer),alpha=0.4)+
-#'        geom_point()
-#' gg + geom_encircle(aes(group=manufacturer,colour=manufacturer))+
-#'        geom_point()
-#'
-#' ss <- subset(mpg,hwy>31 & displ<2)
-#'
-#' gg + geom_encircle(data=ss, colour="blue", s_shape=0.9, expand=0.07) +
-#'   geom_point() + geom_point(data=ss, colour="blue")
+#' gg + geom_encircle_vivi(s_shape=1, expand=0.1, colour="red") + geom_point()
+
 geom_encircle_vivi <- function(mapping = NULL, data = NULL, stat = "identity",
                           position = "identity", na.rm = FALSE, show.legend = NA,
                           inherit.aes = TRUE, ...) {
